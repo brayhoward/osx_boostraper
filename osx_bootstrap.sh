c6b05e82035eed2139c54d812f8b9ae6606de188
Brandon R. Howard <brandonrhoward@gmail.com>
2020-04-14 09:56:07 -0500

    update brew install command

--- osx_bootstrap.sh
+++ osx_bootstrap.sh
@@ -41,7 +41,7 @@ cd $HOME
 # Check for Homebrew, install if we don't have it
 if test ! $(which brew); then
   echo "Installing homebrew..."
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 fi
 
 # Update homebrew recipes
